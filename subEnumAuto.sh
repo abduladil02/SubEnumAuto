1be23f8f9f739a8f044a5b15cb3f87f96e76dd91
Abdul Adil <AbdulAdil02@gmail.com>
2019-11-11 18:35:22 +0530

    script to run nmap,amass and host on linux system at one go

--- subEnumAuto.sh
+++ subEnumAuto.sh
@@ -8,5 +8,7 @@ echo "Running amass intel active scan:"
 amass intel -active -whois -d $1
 echo "Running amass enum active scan"
 amass enum -active -d $1
+echo  "Running nmap on targer"
+nmap -sC -sS -sV -p- -T4 $1
 #Still in development
 
