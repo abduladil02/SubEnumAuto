74536e46e6bc37a7d20f8b0d4e91c63625f0c5cd
Abdul Adil <AbdulAdil02@gmail.com>
2019-11-11 18:51:13 +0530

    .

--- subEnumAuto.sh
+++ subEnumAuto.sh
@@ -3,11 +3,11 @@
 echo "Running :P"
 echo "-----"
 echo "running simple host agaist $1"
-#host -a $1
+host -a $1
 echo "Running amass intel active scan:"
-#amass intel -active -whois -d $1
+amass intel -active -whois -d $1
 echo "Running amass enum active scan"
-#amass enum -active -d $1
+amass enum -active -d $1
 echo  "Running nmap on target"
 nmap -sC -sS -sV -p- -T4 $1
 #Still in development
