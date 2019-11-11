9968cec405ecb98ec28c06110f6dd3b28090e475
Abdul Adil <AbdulAdil02@gmail.com>
2019-11-11 18:33:41 +0530

    script to run amass,nmap and host

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # SubEnumAuto
 Script to save time while running tools Nmap, Amass in one go!
+developed while learning bash
---
+++ subEnumAuto.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Running :P"
+echo "-----"
+echo "running simple host agaist $1"
+host -a $1
+echo "Running amass intel active scan:"
+amass intel -active -whois -d $1
+echo "Running amass enum active scan"
+amass enum -active -d $1
+#Still in development
+
